2cc9c14b548adb20419d3bf810443fcef2925b47
lbsauer <186341046+lbsauer@users.noreply.github.com>
2025-06-10 14:18:25 -0300

    leetcode: 1791

---
+++ leetcode/1601-1800/1791-e/solution.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-center-of-star-graph
+// 2024/11/08
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+};
